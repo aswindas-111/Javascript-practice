72a9ef4e794c16b5095e32f9a0948fa41a9061bb
ASWINDAS <cvaswin1997@gmail.com>
2023-12-09 11:08:59 +0530

    conditional statement-if else practice

--- index.js
+++ index.js
@@ -1,3 +1,11 @@
-console.log(a)
-var a=10;
-console.log(a)
+let country = "india"
+let age = 18
+let gender = "women"
+
+if(age >= 18 && country == "india" && gender == "women")
+{
+    document.write("get married")
+}
+else{
+    document.write("not get married")
+}

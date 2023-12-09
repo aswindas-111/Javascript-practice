3b96758eb8e0f649885d384b38b1d6780703e539
ASWINDAS <cvaswin1997@gmail.com>
2023-12-09 11:34:45 +0530

    else if -problem

--- index.js
+++ index.js
@@ -1,3 +1,5 @@
+//if else
+
 let country = "india"
 let age = 18
 let gender = "women"
@@ -8,4 +10,18 @@ if(age >= 18 && country == "india" && gender == "women")
 }
 else{
     document.write("not get married")
+}
+
+
+//else if
+
+let num = 13
+if (num%2 == 0){
+    document.write("num is divided by 2")
+}
+else if (num%5 == 0){
+    document.write("num is dided by 5")
+}
+else{
+    document.write("invalid")
 }

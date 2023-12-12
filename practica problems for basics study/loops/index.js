69f202c49eb5267916f56522df3011c582cf192f
ASWINDAS <cvaswin1997@gmail.com>
2023-12-12 09:40:24 +0530

    for loop inside a loop

--- practica problems for basics study/loops/index.js
+++ practica problems for basics study/loops/index.js
@@ -6,3 +6,16 @@ for (i=0;i<programming_language.length;i++)
     console.log(programming_language[i])
     
 }
+
+//for loop inside a loop
+
+for(let i=1;i<=5;i++)
+{
+    console.log(i)
+    
+    for(j=1;j<=5;j++)
+    {
+        console.log("hi " + j)
+    }
+    
+}

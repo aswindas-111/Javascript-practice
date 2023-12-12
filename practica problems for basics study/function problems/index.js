89c687a05d666a1b6129d42f92de5cc19402fbbd
ASWINDAS <cvaswin1997@gmail.com>
2023-12-12 15:10:06 +0530

    callback function

--- practica problems for basics study/function problems/index.js
+++ practica problems for basics study/function problems/index.js
@@ -9,3 +9,17 @@ function fn1(a){
 }
 let result = fn1(5);
 console.log(result(5))
+
+
+//callback function 
+
+function sum(result){
+    console.log(result)
+}
+
+function add(a,b,functn){
+    total = a+b;
+    functn(total)
+}
+
+add(10,500,sum)
